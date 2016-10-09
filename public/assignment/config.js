f07b50640d84c89b7ebdf3adfd24f91b5389fafd
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-10-08 18:09:05 -0700

    Moving to angular

---
+++ public/assignment/config.js
@@ -0,0 +1,31 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+
+    function  Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController"
+            })
+            .when("/profile", {
+                templateUrl: "views/user/profile.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            })
+            .when("/website", {
+                templateUrl: "views/website/website-list.view.client.html"
+            })
+            .when("/website/edit", {
+                templateUrl: "views/website/website-edit.view.client.html"
+            })
+            .when("/website/new", {
+                templateUrl: "views/website/website-new.view.client.html"
+            })
+            .otherwise({
+                redirectTo: "views/user/login.view.client.html"
+            })
+    }
+})
