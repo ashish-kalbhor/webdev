7f66ef85d90012da55d22ef92a418e5d5fd0391c
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-10-10 22:34:55 -0700

    Updating config.js

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -10,22 +10,22 @@
                 controller: "LoginController"
             })
             .when("/profile", {
-                templateUrl: "views/user/profile.view.client.html"
+                templateUrl: "/views/user/profile.view.client.html"
             })
             .when("/register", {
-                templateUrl: "views/user/register.view.client.html"
+                templateUrl: "/views/user/register.view.client.html"
             })
             .when("/website", {
-                templateUrl: "views/website/website-list.view.client.html"
+                templateUrl: "/views/website/website-list.view.client.html"
             })
-            .when("/website/edit", {
-                templateUrl: "views/website/website-edit.view.client.html"
+            .when("/website/:wid/edit", {
+                templateUrl: "/views/website/website-edit.view.client.html"
             })
-            .when("/website/new", {
-                templateUrl: "views/website/website-new.view.client.html"
+            .when("/website/:wid/new", {
+                templateUrl: "/views/website/website-new.view.client.html"
             })
             .otherwise({
-                redirectTo: "views/user/login.view.client.html"
+                redirectTo: "/views/user/login.view.client.html"
             })
     }
 })
