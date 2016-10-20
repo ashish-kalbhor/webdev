bdd71fcfb3ff7bebfc9ca0df83a9f00b97da935e
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-10-19 23:35:09 -0700

    Widget list working

--- public/assignment/views/widgets/widget.controller.client.js
+++ public/assignment/views/widgets/widget.controller.client.js
@@ -6,8 +6,30 @@
         .controller("EditWidgetController", EditWidgetController);
 
 
-    function WidgetListController($scope) {
-        var vm = this;
+    function WidgetListController($routeParams, WidgetService, $sce) {
+        var vm  = this;
+        vm.userId  = $routeParams['uid'];
+        vm.websiteId  = $routeParams['wid'];
+        vm.pageId  = $routeParams['pid'];
+
+        vm.checkSafeHtml = checkSafeHtml;
+        vm.checkSafeYouTubeUrl = checkSafeYouTubeUrl;
+
+        function init() {
+            vm.widgets = WidgetService.findWidgetsByPageId(vm.pageId);
+        }
+        init();
+
+        function checkSafeHtml(html) {
+            return $sce.trustAsHtml(html);
+        }
+
+        function checkSafeYouTubeUrl(url) {
+            var parts = url.split('/');
+            var id = parts[parts.length - 1];
+            url = "https://www.youtube.com/embed/"+id;
+            return $sce.trustAsResourceUrl(url);
+        }
 
     }
 
