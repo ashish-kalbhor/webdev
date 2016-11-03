290eaa06f07bdd1666fd67156e23c197c8550369
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-11-02 19:37:10 -0700

    Adding Image upload. Incomplete.

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -14,6 +14,7 @@ module.exports = function (app) {
     app.get('/api/widget/:wgid', findWidgetById);
     app.put('/api/widget/:wgid', updateWidget);
     app.delete('/api/widget/:wgid', deleteWidget);
+    app.post ("/api/upload", upload.single('myFile'), uploadImage);
 
     function createWidget(req, res) {
         var widget = req.body;
@@ -71,5 +72,19 @@ module.exports = function (app) {
         }
         res.send('0');
     }
+
+    function uploadImage(req, res) {
+        var widgetId      = req.body.widgetId;
+        var width         = req.body.width;
+        var myFile        = req.file;
+
+        var originalname  = myFile.originalname; // file name on user's computer
+        var filename      = myFile.filename;     // new file name in upload folder
+        var path          = myFile.path;         // full path of uploaded file
+        var destination   = myFile.destination;  // folder where file is saved to
+        var size          = myFile.size;
+        var mimetype      = myFile.mimetype;
+    }
+
 
 };
