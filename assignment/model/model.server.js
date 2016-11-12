114a8381fe8f01becfe5d621bcd36f7abeb2864d
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-11-11 23:59:03 -0800

    Updating model server

--- assignment/model/model.server.js
+++ assignment/model/model.server.js
@@ -1,16 +1,11 @@
-var express = require('express');
-var app = express();
+module.exports = function () {
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+    var models = {
+        userModel: require("./user/user.model.server")(),
+        websiteModel: require("./website/website.model.server")(),
+        pageModel: require("./page/page.model.server")(),
+        widgetModel: require("./widget/widget.model.server")()
+    };
 
-// configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
-
-require ("./test/app.js")(app);
-require ("./assignment/app.js")(app);
-
-app.set('ipaddress', (process.env.IP));
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'), app.get('ipaddress'));
+    return models;
+};
