e77099ae90dc2cc21a8b1fd32967ba01e750e72f
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-11-27 23:39:07 -0500

    Adding mongolab connection string

--- assignment/model/model.server.js
+++ assignment/model/model.server.js
@@ -1,7 +1,17 @@
 module.exports = function () {
 
+    var connectionString = 'mongodb://heroku_gb8bxpqj:e9erkuff8cuet1t0e621jepnuv@ds033116.mlab.com:33116/heroku_gb8bxpqj';
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+
     var mongoose = require("mongoose");
-    mongoose.connect("mongodb://localhost/cs5610-assignment");
+    //mongoose.connect("mongodb://localhost/cs5610-assignment");
+    mongoose.connect(connectionString);
 
     var models = {
         userModel: require("./user/user.model.server")(),
