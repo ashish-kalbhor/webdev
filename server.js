815fe1365f1a4830fd93b817e59406ac10d795eb
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-12-01 23:22:30 -0800

    Updating Package info for passport.

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var app = express();
+var multer        = require('multer');
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+var mongoose      = require('mongoose');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/webappmaker');
-// configure a public directory to host static content
+multer();
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
 //require ("./test/app.js")(app);
