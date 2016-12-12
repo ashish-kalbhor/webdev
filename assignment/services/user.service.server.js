bd3e68c5239afc7b094c5bb3838564e7743bdf2c
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2016-12-12 00:15:23 -0800

    App fix for heroku

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -79,6 +79,7 @@ module.exports = function (app, WebAppModels) {
                     } else {
                         var names = profile.displayName.split(" ");
                         var newFacebookUser = {
+                            username: names[0],
                             lastName:  names[1],
                             firstName: names[0],
                             email:     profile.emails ? profile.emails[0].value : "",
@@ -126,6 +127,7 @@ module.exports = function (app, WebAppModels) {
                         return done(null, user);
                     } else {
                         var newGoogleUser = {
+                            username: profile.name.givenName,
                             lastName: profile.name.familyName,
                             firstName: profile.name.givenName,
                             email: profile.emails[0].value,
